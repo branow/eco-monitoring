f0a0cf8be5a0bb8bd97e559eba6066e4e1adce78
branow <bodnarorestdraw@gmail.com>
2023-09-25 19:16:17 +0300

    create table-function.js: create function hide id column

---
+++ src/main/resources/static/js/table-functions.js
@@ -0,0 +1,47 @@
+/*Set property (style.display = 'none') for all cells of the columns
+which names contains substring 'Id'*/
+function hideTableIdColumns(table) {
+    let indexes = [];
+    let columns = table.querySelectorAll('thead > tr > td');
+    for (let i = 0, j = 0; i<columns.length; i++) {
+        if (columns[i].innerText.includes('Id')) {
+            indexes[j] = i;
+            j++;
+        }
+    }
+    for (let index in indexes) {
+        hideTableColumnByIndex(table, index);
+    }
+}
+
+
+/*Set property (style.display = 'none') for all cells of the column
+which name equals to the given one.*/
+function hideTableColumnByName(table, columnName) {
+    let index = numberOfColumn(table, columnName);
+    hideTableColumnByIndex(table, index);
+}
+
+/*Set property (style.display = 'none') for all cells of the column
+which index equals to the given one.*/
+function hideTableColumnByIndex(table, index) {
+    let headersCells = table.querySelectorAll('thead > tr > td');
+    headersCells[index].style.display = 'none';
+    let rows = table.querySelectorAll('tbody > tr');
+    for (let i = 0; i<rows.length; i++) {
+        let cells = rows[i].querySelectorAll('td');
+        cells[index].style.display = 'none';
+    }
+}
+
+
+/*Return the number of the column which name equals to the given one.
+Numbers starts from zero.*/
+function numberOfColumn(table, columnName) {
+    let columns = table.querySelectorAll('thead > tr > td');
+    for (let i = 0; i<columns.length; i++) {
+        if (columns[i].innerText === columnName) {
+            return i;
+        }
+    }
+}
