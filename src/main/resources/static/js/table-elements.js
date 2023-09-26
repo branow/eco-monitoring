0626e97f3d53784adabac705e331edbd92d6ae86
branow <bodnarorestdraw@gmail.com>
2023-09-26 15:28:58 +0300

    modify table-elements.js: add function createDeleteRowButton

--- src/main/resources/static/js/table-elements.js
+++ src/main/resources/static/js/table-elements.js
@@ -1,77 +1,14 @@
 
 
-function configureTable(table) {
-    hideTableIdColumns(table);
-    addNonVisibleDeleteRowButtons(table, deleteButton());
-}
-
-
-function deleteButton(success, error) {
+//returns element - delete row button
+function createDeleteRowButton(error) {
     let button = document.createElement("button");
     button.innerText = "del";
-    button.className = "delete-button";
+    button.className = "del";
     button.onclick = function (event) {
-        let row = event.target.parentElement.parentElement;
-        deleteRow(row, success, error);
+        deleteRowButtonClickAction(event, error)
     }
     return button;
-}
-
-
-function deleteRow(row, success, error) {
-    console.log(row.target.parentElement);
-    let table = row.target.parentElement.parentElement;
-    if (table.dataset.type === 'companies') {
-        deleteCompany(readCompany(row).companyId, success, error);
-    } else if (table.id === 'pollutants') {
-        deletePollutant(readPollutant(row).companyId, success, error);
-    } else if (table.id === 'pollutions') {
-        deletePollution(readPollution(row).companyId, success, error);
-    }
-}
-
-function readData(row) {
-    let table = row.target.parentElement.parentElement;
-    if (table.dataset.type === 'companies') {
-        return readCompany(row);
-    } else if (table.id === 'pollutants') {
-        return readPollutant(row);
-    } else if (table.id === 'pollutions') {
-        return readPollution(row);
-    }
-}
-
-function readCompany(row) {
-    let cells = row.querySelectorAll("td");
-    return {
-        'companyId':cells[0].innerText,
-        'companyName':cells[1].innerText,
-        'ownership':cells[2].innerText,
-        'economicActivity':cells[3].innerText,
-        'address':cells[4].innerText,
-    }
-}
-
-function readPollutant(row) {
-    let cells = row.querySelectorAll("td");
-    return {
-        'pollutantId':cells[0].innerText,
-        'pollutantName':cells[1].innerText,
-        'massConsumption':cells[2].innerText,
-        'gdk':cells[3].innerText,
-        'hazardClass':cells[4].innerText,
-    }
-}
-
-function readPollution(row) {
-    let cells = row.querySelectorAll("td");
-    return {
-        'pollutionId':cells[0].innerText,
-        'company':cells[1].dataset.id,
-        'pollutant':cells[2].dataset.id,
-        'emissionMass':cells[3].innerText,
-        'year':cells[4].innerText,
-    }
 }
 
 
