1467914918473207eb7aee4a0f4ac373ec97e6d4
branow <bodnarorestdraw@gmail.com>
2023-09-27 20:57:27 +0300

    create class PollutionModel

---
+++ src/main/java/com/brano/ecomonitor/model/PollutionModel.java
@@ -0,0 +1,20 @@
+package com.brano.ecomonitor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PollutionModel {
+
+    private Long pollutionId;
+    private Double emissionMass;
+    private Integer year;
+    private Long company;
+    private Long pollutant;
+
+}
